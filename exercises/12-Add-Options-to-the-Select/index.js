8d2ce018db8d44d297eb0c779b77e407ca777adf
donpablohdf <pabloalegrejuan@gmail.com>
2022-09-28 17:12:11 +0000

    hecho ejercicio 12

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -2,10 +2,11 @@ let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Vene
 let INPUT = document.getElementById("mySelect");
 INPUT.addEventListener("change", function() {
 	//your code here
-    alert("cambia");
-	var option = document.createElement("option");
-  	option.appendChild(document.createTextNode("Four"));
-  	option.setAttribute("value", "Hoo"); // added line
-  	INPUT.appendChild(option);
+   countries.forEach(function(pais){
+		var option = document.createElement("option");
+		option.appendChild(document.createTextNode(pais));
+		option.setAttribute("value", pais); // added line
+		INPUT.appendChild(option);
+	})
 	
 });
