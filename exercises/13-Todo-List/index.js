5899a696d2e91a6097c1697f0c7e72a83d146dd1
donpablohdf <pabloalegrejuan@gmail.com>
2022-09-28 18:18:03 +0000

    crear addtask del ejercicio 13

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,22 @@
 // Your code here
+const deleteTask = () =>{
+
+}
+const addTask = (valor) =>{
+    alert("Crear elemento")
+    let ul = document.querySelector("ul")
+	let li = document.createElement("li")
+	let span = document.createElement("span")
+	let i = document.createElement("i")
+	i.setAttribute("class", "fa fa-trash")
+	ul.appendChild(li) //metemos li dentro de ul
+	li.appendChild(span) //metemos span dentro de li
+	span.appendChild(i) // metemos i dentro de span
+	li.appendChild(document.createTextNode(valor)) //metemos el texto en li
+}
+let INPUT = document.querySelector("input")
+INPUT.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') { //si pulsamos enter al cambiar el texto del input
+        addTask(INPUT.value);
+    }
+})
